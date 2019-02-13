15d3c96483e8b6d09bd20d5fdf39beafff41fdfa
Theodore Chuang <chuangtheodore@gmail.com>
2019-02-13 18:58:29 +1100

    Doc-Tutorial: add basic test for Question model, Poll index view, Poll detail view

--- django-docs-tutorial/venv/mysite/polls/views.py
+++ django-docs-tutorial/venv/mysite/polls/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
 from django.views import generic
+from django.utils import timezone
 
 from .models import Question, Choice
 
@@ -12,13 +13,19 @@ class IndexView(generic.ListView):
     context_object_name = 'latest_question_list'
 
     def get_queryset(self):
-        """Return the last five published questions."""
-        return Question.objects.order_by('-pub_date')[:5]
+        """Return the last five published questions. (not including future ones)"""
+        return Question.objects.filter(
+            pub_date__lte=timezone.now()
+        ).order_by('-pub_date')[:5]
 
 
 class DetailView(generic.DetailView):
     model = Question
     template_name = 'polls/detail.html'
+
+    def get_queryset(self):
+        # Excludes any questions that are not published yet
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
 
 class ResultsView(generic.DetailView):
@@ -79,5 +86,3 @@ def vote(request, question_id):
         # with POST data. This prevents data from being posted twice if a
         # user hits the Back button.
         return HttpResponseRedirect(reverse('polls:results', args=(question_id,)))
-
-
